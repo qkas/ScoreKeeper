6fea1d1889879bd679011693ebc64c7365b1329b
qkas <kassuvaa@gmail.com>
2024-12-05 17:58:42 +0200

    Improved HomeScreen looks

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,56 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Welcome to the Notes App!</Text>
-      <Button
-        title="Create New Note"
+    <View style={styles.container}>
+      {/* NEW SCOREBOARD BUTTON */}
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => navigation.navigate('NewScore')}
-      />
+      >
+        <Text style={styles.buttonText}>New scoreboard</Text>
+      </TouchableOpacity>
+      {/* SCOREBOARD ENTRIES */}
+      <Text style={styles.header}>Scoreboards</Text>
+      {/* TODO */}
     </View>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111',
+    padding: 20,
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: 80,
+    marginBottom: 50,
+    backgroundColor: '#6200ea',
+    paddingVertical: 14,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  header: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    textAlign: 'center',
+    borderBottomColor: 'grey',
+    borderBottomWidth: 3,
+    width: '100%',
+  },
+});
