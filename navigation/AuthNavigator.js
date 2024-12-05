d32acb91164dbaa72aa2afb27f54ec1635160856
qkas <kassuvaa@gmail.com>
2024-12-05 17:11:24 +0200

    Added StatusBar to all screens

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { StatusBar, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
@@ -7,23 +7,29 @@ import RegisterScreen from '../screens/RegisterScreen';
 const Stack = createStackNavigator();
 
 const AuthNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: { backgroundColor: '#000' },
-      headerTintColor: '#fff',
-      headerTitleStyle: { fontSize: 20, fontWeight: 'bold' },
-    }}>
-    <Stack.Screen
-      name="Login"
-      component={LoginScreen}
-      options={{ title: 'Login to your account' }}
+  <>
+    <StatusBar
+      translucent={true}
+      barStyle="light-content"
     />
-    <Stack.Screen
-      name="Register"
-      component={RegisterScreen}
-      options={{ title: 'Login to your account' }}
-    />
-  </Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#000' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontSize: 20, fontWeight: 'bold' },
+      }}>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ title: 'Login to your account' }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ title: 'Login to your account' }}
+      />
+    </Stack.Navigator>
+  </>
 );
 
 export default AuthNavigator;
--- navigation/HomeNavigator.js
+++ navigation/HomeNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { StatusBar, TouchableOpacity, StyleSheet, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import NewScoreScreen from '../screens/NewScoreScreen';
@@ -11,30 +11,36 @@ const HomeNavigator = () => {
   const { logout } = useAuth();
 
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: '#000' },
-        headerTintColor: '#fff',
-        headerTitleStyle: { fontSize: 20, fontWeight: 'bold' },
-      }}>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Scoreboard overview',
-          headerRight: () => (
-            <TouchableOpacity style={styles.logoutButton} onPress={logout}>
-              <Text style={styles.logoutText}>Logout</Text>
-            </TouchableOpacity>
-          ),
-        }}
+    <>
+      <StatusBar
+        translucent={true}
+        barStyle="light-content"
       />
-      <Stack.Screen
-        name="NewScore"
-        component={NewScoreScreen}
-        options={{ title: "Create a new scoreboard" }}
-      />
-    </Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#000' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontSize: 20, fontWeight: 'bold' },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Scoreboard overview',
+            headerRight: () => (
+              <TouchableOpacity style={styles.logoutButton} onPress={logout}>
+                <Text style={styles.logoutText}>Logout</Text>
+              </TouchableOpacity>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="NewScore"
+          component={NewScoreScreen}
+          options={{ title: "Create a new scoreboard" }}
+        />
+      </Stack.Navigator>
+    </>
   );
 };
 
