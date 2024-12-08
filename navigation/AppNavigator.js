4b23f72307f7d747fcfc0a23e1731a47e3676841
Kasperi Jaakkola <kassuvaa@gmail.com>
2024-12-08 18:41:14 +0200

    Seperated loading screen and fixed its margin

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './AuthNavigator';
 import HomeNavigator from './HomeNavigator';
+import LoadingScreen from '../screens/LoadingScreen';
 import { useAuth } from '../context/AuthContext';
-
-const LoadingScreen = () => (
-  <View style={styles.loadingContainer}>
-    <ActivityIndicator size="large" color="#6200ea" />
-    <Text style={styles.loadingText}>Loading...</Text>
-  </View>
-);
 
 const AppNavigator = () => {
   const { isAuthenticated, loading } = useAuth();
@@ -27,19 +20,3 @@ const AppNavigator = () => {
 };
 
 export default AppNavigator;
-
-const styles = StyleSheet.create({
-  loadingContainer: {
-    margin: '4%',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#111',
-  },
-  loadingText: {
-    marginTop: 10,
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
