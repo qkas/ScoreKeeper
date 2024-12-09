97001b23005962b2272d9ed389ff6b2c19b49332
Kasperi Jaakkola <kassuvaa@gmail.com>
2024-12-09 06:20:09 +0200

    Fix indentation to 2 spaces for consistency

--- util/helper.js
+++ util/helper.js
@@ -2,45 +2,45 @@ import axios from 'axios';
 const baseUrl = 'https://scorekeeper-d2fe7-default-rtdb.europe-west1.firebasedatabase.app';
 
 const postData = async (data) => {
-    try {
-        const response = await axios.post(`${baseUrl}/scoreboards.json`, data);
-        return response.data;
-    } catch (error) {
-        console.log(error.response.data);
-        if (error.response) {
-            alert(error.response.data.error.message);
-        } else {
-            alert('An unknown error occurred');
-        }
+  try {
+    const response = await axios.post(`${baseUrl}/scoreboards.json`, data);
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    if (error.response) {
+      alert(error.response.data.error.message);
+    } else {
+      alert('An unknown error occurred');
     }
+  }
 }
 
 const getData = async () => {
-    try {
-        const response = await axios.get(`${baseUrl}/scoreboards.json`);
-        return response.data;
-    } catch (error) {
-        console.log(error.response.data);
-        if (error.response) {
-            alert(error.response.data.error.message);
-        } else {
-            alert('An unknown error occurred');
-        }
+  try {
+    const response = await axios.get(`${baseUrl}/scoreboards.json`);
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    if (error.response) {
+      alert(error.response.data.error.message);
+    } else {
+      alert('An unknown error occurred');
     }
+  }
 }
 
 const deleteData = async (id) => {
-    try {
-        const response = await axios.post(`${baseUrl}/games/${id}.json`);
-        return response.data;
-    } catch (error) {
-        console.log(error.response.data);
-        if (error.response) {
-            alert(error.response.data.error.message);
-        } else {
-            alert('An unknown error occurred');
-        }
+  try {
+    const response = await axios.post(`${baseUrl}/games/${id}.json`);
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    if (error.response) {
+      alert(error.response.data.error.message);
+    } else {
+      alert('An unknown error occurred');
     }
+  }
 }
 
 export { postData, getData, deleteData };
